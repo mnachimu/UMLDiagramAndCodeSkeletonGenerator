8feebf73d0b702f18dbb61c1c15b56e9fcdccf05
Mugunth <mnachimu@asu.edu>
2022-11-19 19:01:52 -0700

    Initial Commit

---
+++ GUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends JFrame implements ActionListener {
+
+    public GUI() {
+        this.setTitle("UML diagram and code skeleton generator V1");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
---
+++ UMLDiagramAndSkeletonGenerator.java
@@ -0,0 +1,9 @@
+public class UMLDiagramAndSkeletonGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        GUI plotPanel = new GUI();
+    }
+
+}
