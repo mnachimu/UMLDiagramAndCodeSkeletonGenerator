5ec9b70863bed5b2f11794ad7ef20fde727439cb
akwon20 <austin.kwon2016@gmail.com>
2022-11-23 12:10:20 -0700

    Kwon: implemented mouseClicked

--- CanvasPanel.java
+++ CanvasPanel.java
@@ -3,30 +3,22 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class CanvasPanel extends JPanel implements MouseListener, MouseMotionListener {
-
+	
 	public CanvasPanel(int x, int y) {
 		this.setBackground(Color.white);
 		this.setBounds(x, y, 600, 550);
     	this.setLayout(new BorderLayout());
     	this.setBorder(BorderFactory.createLineBorder(Color.black, 1));
+    	
+    	addMouseListener(this);
+    	addMouseMotionListener(this);
 	}
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Mouse clicked at ("
+				+ e.getX() + ", " + e.getY() + ")");
 	}
 
 	@Override
@@ -37,6 +29,18 @@ public class CanvasPanel extends JPanel implements MouseListener, MouseMotionLis
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
