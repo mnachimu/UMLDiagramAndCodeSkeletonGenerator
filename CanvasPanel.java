a65213cf07baa9295117f4f44417f6dc3c3636a6
akwon20 <austin.kwon2016@gmail.com>
2022-11-28 21:25:26 -0700

    Drew ClassObjects on click

--- CanvasPanel.java
+++ CanvasPanel.java
@@ -1,25 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.ArrayList;
 
 public class CanvasPanel extends JPanel implements MouseListener, MouseMotionListener {
 	
 	List<ClassObject> objects;
+	private static final int width = 625;
+	private static final int height = 600;
+	
+	public BufferedImage canvas;
+	
+	int mouseX, mouseY;
 	
 	public CanvasPanel(int x, int y) {
+		
+		canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		objects = new ArrayList<ClassObject>();
+		
 		this.setBackground(Color.white);
-		this.setBounds(x, y, 625, 600);
+		this.setBounds(x, y, width, height);
     	this.setLayout(new BorderLayout());
     	this.setBorder(BorderFactory.createLineBorder(Color.black, 1));
     	
     	addMouseListener(this);
     	addMouseMotionListener(this);
+    	
 	}
 	
+	@Override
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+		 super.paintComponent(g);
+		 
+		 g.setColor(new Color(242, 213, 145));
+		 
+		 // FIXME: Exception when uncommented
+		for (int i = 0; i < objects.size(); i++) {
+				 objects.get(i).draw(g);
+		}
+
+			 
+//		g.drawImage(canvas, 0, 0, this);
 	}
 	
 	@Override
@@ -28,6 +51,7 @@ public class CanvasPanel extends JPanel implements MouseListener, MouseMotionLis
 		System.out.println("Mouse clicked at ("
 				+ e.getX() + ", " + e.getY() + ")");
 		ClassObject o = new ClassObject(e.getX(), e.getY());
+		objects.add(o);
 		
 		repaint();
 	}
@@ -35,13 +59,15 @@ public class CanvasPanel extends JPanel implements MouseListener, MouseMotionLis
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		repaint();
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		repaint();
+
 	}
 
 	@Override
